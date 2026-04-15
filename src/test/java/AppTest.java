54b34de7cbc9896b400b04d797fa374ff520f5b4
Varun C M <vcm766737@gmail.com>
2026-04-15 13:51:27 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,10 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     @Test 
     public void testAppHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should exist", classUnderTest);
+        assertNotNull(classUnderTest.getGreeting());
     }
 }
